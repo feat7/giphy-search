7e4fb07675fd0225ab74acb32cd5f27a30fc348e
feat7 <vinaykhobragade99@gmail.com>
2018-11-03 01:00:04 +0530

    add: Readme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
             return (
               <div key={index}>
                 {item.title}
-                <img src={item.images.original.url} height={80} width={80} />
+                <img src={item.images.original.url} alt={item.title} height={80} width={80} />
               </div>
             );
           })}
